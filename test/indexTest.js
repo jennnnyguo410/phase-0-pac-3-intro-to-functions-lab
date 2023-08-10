f10e0457a1fa0253cf4e1f8ce5c5578f23631772
Hao Guo <jennnnyguo@gmail.com>
2023-08-10 21:58:24 +1000

    commit tree 5eb7c4f77ea9d69e7138522f96633dd40c3b9538

--- test/indexTest.js
+++ test/indexTest.js
@@ -7,9 +7,7 @@ describe('shout(string)', function () {
   })
 })
 
-function shout(string) {
-  return string.toUpperCase()
-}
+
 
 describe('whisper(string)', function () {
   it('receives one argument and returns it in all lowercase', function () {
@@ -17,9 +15,6 @@ describe('whisper(string)', function () {
   })
 })
 
-function whisper(string) {
-  return string.toLowerCase()
-}
 
 describe('logShout(string)', function () {
   it('takes a string argument and logs it in all caps using console.log()', function () {
@@ -33,9 +28,7 @@ describe('logShout(string)', function () {
   })
 })
 
-function logShout(string) {
-  console.log(string.toUpperCase())
-}
+
 
 describe('logWhisper(string)', function () {
   it('takes a string argument and logs it in all lowercase using console.log()', function () {
@@ -49,9 +42,7 @@ describe('logWhisper(string)', function () {
   })
 })
 
-function logWhisper(string) {
-  console.log(string.toLowerCase())
-}
+
 
 describe('sayHiToHeadphonedRoommate(string)', function () {
   it('returns "I can\'t hear you!" if `string` is lowercase', function () {
@@ -69,16 +60,4 @@ describe('sayHiToHeadphonedRoommate(string)', function () {
   });
 })
 
-function sayHiToHeadphonedRoommate(string) {
-  if (string.toLowerCase() === string) {
-    return "I can't hear you!"
-  }
 
-  if (string.toUpperCase() === string) {
-    return "YES INDEED!"
-  }
-
-  if (string === "Let's have dinner together!") {
-    return "I would love to!"
-  }
-}
